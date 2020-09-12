48754c9814a31a7199c04f731b78b633965ebbe8
Xiexe <Jcmaddox1@gmail.com>
2020-09-12 18:05:19 -0400

    finish up the app

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,12 +1,21 @@
+
+const resetSound = new Audio('./assets/sound/reset.ogg');
+const drawingSound = new Audio('./assets/sound/drawloop.ogg');
+
 var initialized = false;
 var isDrawing = false;
 var isEraser = false;
-let x = 0;
-let y = 0;
+var x = 0;
+var y = 0;
 var strokeWidth = 6;
 var strokeColor = 'white';
-var resetSound = new Audio('./assets/sound/reset.ogg');
-var drawingSound = new Audio('./assets/sound/drawloop.ogg');
+var eraserButton;
+var pencilButton;
+var thinStrokeButton;
+var thickStrokeButton;
+var selectedButton;
+var inactiveButton;
+var canvasRect;
 
 function playButtonSound()
 {
@@ -17,29 +26,52 @@ function playButtonSound()
 function resetCanvas()
 {
     var canvas = document.getElementById('canvas');
+    eraserButton = document.getElementById('eraser');
+    pencilButton = document.getElementById('pencil');
+    thinStrokeButton = document.getElementById('thinstroke');
+    thickStrokeButton = document.getElementById('largestroke');
+    selectedButton = document.getElementById('selected-colors');
+    inactiveButton = document.getElementById('inactive-colors');
+    canvasRect = document.getElementById('canvas-bg');
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight
     drawCanvas();
     isDrawing = false;
     isEraser = false;
-    setSize(0);
-    selectTool(0);
 }
 
 function selectTool(toolIndex)
 {
     if(toolIndex === 0)
-        strokeColor = 'white';
-    if(toolIndex === 1)
+    {
+        pencilButton.style.backgroundColor = window.getComputedStyle(selectedButton).getPropertyValue('background-color');
+        eraserButton.style.backgroundColor = inactiveButton.style.backgroundColor;
         strokeColor = 'black';
+    }
+    if(toolIndex === 1)
+    {
+        pencilButton.style.backgroundColor = inactiveButton.style.backgroundColor;
+        eraserButton.style.backgroundColor = window.getComputedStyle(selectedButton).getPropertyValue('background-color');
+        strokeColor = window.getComputedStyle(canvasRect).getPropertyValue('background-color');
+    }
 }
 
 function setSize(size)
 {
     if(size === 0)
+    {
+        thinStrokeButton.style.backgroundColor = window.getComputedStyle(selectedButton).getPropertyValue('background-color');
+        thickStrokeButton.style.backgroundColor = inactiveButton.style.backgroundColor;
         strokeWidth = 6;
+    }
+
     if(size === 1)
+    {
+        thinStrokeButton.style.backgroundColor = inactiveButton.style.backgroundColor;
+        thickStrokeButton.style.backgroundColor = window.getComputedStyle(selectedButton).getPropertyValue('background-color');
         strokeWidth = 16;
+    }
 }
 
 function drawCanvas()
@@ -49,8 +81,6 @@ function drawCanvas()
     if (canvas.getContext)
     {
         var ctx = canvas.getContext('2d'); 
-        ctx.fillStyle = "Black";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
     function getPosition(event)
@@ -99,5 +129,7 @@ function drawCanvas()
         canvas.addEventListener('mousemove', function(e){ sketch(canvas, e); });
         initialized = true;
         resetCanvas();
+        setSize(0);
+        selectTool(0);
     }
 }
